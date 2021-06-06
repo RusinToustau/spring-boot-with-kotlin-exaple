82fdacc09ae6dba3c391ac696174a728b5fc4364
Felix Toustau <ftoustau@gmail.com>
2021-06-06 18:14:28 -0300

    Init springboot project

---
+++ src/main/kotlin/com/example/productexample/ProductExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.productexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProductExampleApplication>(*args)
+}
