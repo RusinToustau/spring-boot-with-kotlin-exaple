8c17fcd50a5bbd8b2985af9a84020d19faf9c7e2
Felix Toustau <ftoustau@gmail.com>
2021-06-07 19:38:29 -0300

    compare product list

--- src/test/kotlin/com/example/productexample/ProductExampleApplicationTests.kt
+++ src/test/kotlin/com/example/productexample/ProductExampleApplicationTests.kt
@@ -1,5 +1,11 @@
 package com.example.productexample
 
+import com.example.productexample.product.domain.Product
+import com.example.productexample.product.service.ProductService
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,11 +23,24 @@ class ProductExampleApplicationTests {
 
 	@Autowired
 	private lateinit var mockMvc: MockMvc
+	@Autowired
+	private lateinit var mapper: ObjectMapper
+	@Autowired
+	private lateinit var productService: ProductService
 
 	@Test
 	fun findAll() {
-		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/product"))
+		val jsonResponse = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/product"))
 				.andExpect(status().isOk)
+				.andReturn().response.contentAsString
+
+		val products: List<Product> = mapper.readValue(
+				jsonResponse,
+				mapper.typeFactory.constructCollectionType(List::class.java, Product::class.java)
+		)
+
+		assertThat(products, Matchers.`is`(equalTo(productService.findAll())))
 	}
+
 
 }
